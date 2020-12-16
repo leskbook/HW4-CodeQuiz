7a568cde77b6ac98476d8d4df708affe7e4fb0cb
Leslie Book <lesliek.book@gmail.com>
2020-12-16 18:36:37 -0500

    First commit with initial css script and html

---
+++ Assets/scripts/script.js
@@ -0,0 +1,38 @@
+// Array for questions and multiple choice answers
+
+var questions = [{
+        question: "What does Woody from Toy Story have on his boot?",
+        choices: ["A map of the US", "His home address", "Andy", "Mom"],
+        answer: "Andy"
+    },
+    {
+        question: "Whose tea party does Alice attend?",
+        choices: ["The artist formerly known as Prince", "Kim Kardashian", "President Trump", "The Mad Hatter"],
+        answer: "The Mad Hatter"
+    },
+    {
+        question: "What puts Snow White into a deep sleep?",
+        choices: ["javascripting", "a poison apple", "watching presidential debates", "a date with Sleepy"],
+        answer: "a poison apple"
+    },
+    {
+        question: "What animal swallowed a clock in Peter Pan?",
+        choices: ["a rabid hamster", "a confused beaver", "a crocodile", "a hungry hungry hippo"],
+        answer: "a crocodile"
+    },
+    {
+        question: "What city is nicknamed Sin City?",
+        choices: ["Las Vegas", "Salt Lake City", "Baltimore", "San Francisco"],
+        answer: "Las Vegas"
+    },
+    {
+        question: "What did Amelia Earhart do to make her famous?",
+        choices: ["Won an all you can eat wing contest", "was an accomplished female pilot", "learned javascripting overnight", "was a scuba instructor"],
+        answer: "was an accomplished female pilot"
+    },
+    {
+        question: "Which president made Thanksgiving and annual holiday in November?",
+        choices: ["Obama", "Clinton", "Lincoln", "Washington"],
+        answer: "Lincoln"
+    }
+];
